a413b28a6e696bf12a908efb4f1b03644f1755ca
DouglasGomes0 <douglasgomesaqui@gmail.com>
2025-06-30 15:17:32 -0300

    Initial commit

---
+++ Padarosa/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Padarosa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txbLogin = new System.Windows.Forms.TextBox();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.pibLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(14, 153);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(55, 20);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(12, 227);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(61, 20);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Poor Richard", 40.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(175, 48);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(220, 61);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Padarosa";
+            // 
+            // txbLogin
+            // 
+            this.txbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLogin.Location = new System.Drawing.Point(77, 152);
+            this.txbLogin.Name = "txbLogin";
+            this.txbLogin.Size = new System.Drawing.Size(283, 26);
+            this.txbLogin.TabIndex = 3;
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSenha.Location = new System.Drawing.Point(79, 226);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(281, 26);
+            this.txbSenha.TabIndex = 4;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(79, 281);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(239, 54);
+            this.btnEntrar.TabIndex = 6;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // pibLogo
+            // 
+            this.pibLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pibLogo.Image = global::Padarosa.Properties.Resources.bakery;
+            this.pibLogo.Location = new System.Drawing.Point(12, 12);
+            this.pibLogo.Name = "pibLogo";
+            this.pibLogo.Size = new System.Drawing.Size(157, 115);
+            this.pibLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibLogo.TabIndex = 5;
+            this.pibLogo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(410, 347);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.pibLogo);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.txbLogin);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Padarosa - Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txbLogin;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.PictureBox pibLogo;
+        private System.Windows.Forms.Button btnEntrar;
+    }
+}
+
---
+++ Padarosa/FormMenuPrincipal.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Padarosa
+{
+    partial class FormMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnGestaoProdutos = new System.Windows.Forms.Button();
+            this.btnLancarComandas = new System.Windows.Forms.Button();
+            this.btnGestaoUsuarios = new System.Windows.Forms.Button();
+            this.btnCaixa = new System.Windows.Forms.Button();
+            this.pibLogo = new System.Windows.Forms.PictureBox();
+            this.lblLogado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Poor Richard", 40.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(327, 19);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(220, 61);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Padarosa";
+            // 
+            // btnGestaoProdutos
+            // 
+            this.btnGestaoProdutos.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnGestaoProdutos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestaoProdutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestaoProdutos.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGestaoProdutos.Location = new System.Drawing.Point(229, 106);
+            this.btnGestaoProdutos.Name = "btnGestaoProdutos";
+            this.btnGestaoProdutos.Size = new System.Drawing.Size(324, 50);
+            this.btnGestaoProdutos.TabIndex = 2;
+            this.btnGestaoProdutos.Text = "Gestão de Produtos";
+            this.btnGestaoProdutos.UseVisualStyleBackColor = false;
+            // 
+            // btnLancarComandas
+            // 
+            this.btnLancarComandas.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnLancarComandas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancarComandas.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancarComandas.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLancarComandas.Location = new System.Drawing.Point(230, 195);
+            this.btnLancarComandas.Name = "btnLancarComandas";
+            this.btnLancarComandas.Size = new System.Drawing.Size(324, 50);
+            this.btnLancarComandas.TabIndex = 3;
+            this.btnLancarComandas.Text = "Lançamento de comandas";
+            this.btnLancarComandas.UseVisualStyleBackColor = false;
+            // 
+            // btnGestaoUsuarios
+            // 
+            this.btnGestaoUsuarios.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnGestaoUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestaoUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestaoUsuarios.Location = new System.Drawing.Point(230, 368);
+            this.btnGestaoUsuarios.Name = "btnGestaoUsuarios";
+            this.btnGestaoUsuarios.Size = new System.Drawing.Size(324, 48);
+            this.btnGestaoUsuarios.TabIndex = 4;
+            this.btnGestaoUsuarios.Text = "Gestão de Usuários";
+            this.btnGestaoUsuarios.UseVisualStyleBackColor = false;
+            // 
+            // btnCaixa
+            // 
+            this.btnCaixa.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnCaixa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCaixa.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCaixa.Location = new System.Drawing.Point(229, 285);
+            this.btnCaixa.Name = "btnCaixa";
+            this.btnCaixa.Size = new System.Drawing.Size(324, 48);
+            this.btnCaixa.TabIndex = 5;
+            this.btnCaixa.Text = "Caixa";
+            this.btnCaixa.UseVisualStyleBackColor = false;
+            // 
+            // pibLogo
+            // 
+            this.pibLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pibLogo.Image = global::Padarosa.Properties.Resources.bakery;
+            this.pibLogo.Location = new System.Drawing.Point(186, 0);
+            this.pibLogo.Name = "pibLogo";
+            this.pibLogo.Size = new System.Drawing.Size(135, 100);
+            this.pibLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibLogo.TabIndex = 0;
+            this.pibLogo.TabStop = false;
+            // 
+            // lblLogado
+            // 
+            this.lblLogado.AutoSize = true;
+            this.lblLogado.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogado.Location = new System.Drawing.Point(5, 429);
+            this.lblLogado.Name = "lblLogado";
+            this.lblLogado.Size = new System.Drawing.Size(187, 17);
+            this.lblLogado.TabIndex = 6;
+            this.lblLogado.Text = "Você está logado como: ";
+            // 
+            // FormMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLogado);
+            this.Controls.Add(this.btnCaixa);
+            this.Controls.Add(this.btnGestaoUsuarios);
+            this.Controls.Add(this.btnLancarComandas);
+            this.Controls.Add(this.btnGestaoProdutos);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pibLogo);
+            this.Name = "FormMenuPrincipal";
+            this.Text = "FormMenuPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.pibLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pibLogo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnGestaoProdutos;
+        private System.Windows.Forms.Button btnLancarComandas;
+        private System.Windows.Forms.Button btnGestaoUsuarios;
+        private System.Windows.Forms.Button btnCaixa;
+        private System.Windows.Forms.Label lblLogado;
+    }
+}
